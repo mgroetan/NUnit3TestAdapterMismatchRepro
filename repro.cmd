6dad7503452aff2f6ca1cd7db02b5269eb499f19
Morten Grøtan <9464822+mgroetan@users.noreply.github.com>
2020-09-16 11:30:41 +0200

    Reworked repro script for readability

--- repro.cmd
+++ repro.cmd
@@ -2,20 +2,34 @@
 setlocal
 
 set vstest=vstest.console.exe
-set TestAdapterPathOption=/TestAdapterPath:"%CD%"
-set TestAdapterTrxLoggerOption=/Logger:"trx"
-set RunNetCoreTests=%vstest% "%CD%\ProjectANetCore\bin\Debug\netcoreapp3.1\ProjectANetCore.dll" %TestAdapterPathOption%
-set RunNetFrameworkTests=%vstest% "%CD%\ProjectBNetFramework\bin\Debug\net461\ProjectBNetFramework.dll" %TestAdapterPathOption%
+set RunNetCoreTests=%vstest% "%CD%\ProjectANetCore\bin\Debug\netcoreapp3.1\ProjectANetCore.dll"
+set RunNetFrameworkTests=%vstest% "%CD%\ProjectBNetFramework\bin\Debug\net461\ProjectBNetFramework.dll"
 
-@echo * Verifying runtime environment
-where %vstest%
-if %errorlevel% neq 0 (
-	@echo  - Failed to locate %vstest% - please run through a VS developer command prompt
-	exit /b %errorlevel%
-)
+call :VerifyRuntimeEnvironment
 
-call :RunTests false
-call :RunTests true
+set RunMessage=Running .NET Core tests (without TRX logger)
+set CmdToExec=%RunNetCoreTests% /TestAdapterPath:"%CD%"
+call :LogAndExecuteCommand "%RunMessage%" "%CmdToExec%"
+
+set RunMessage=Running .NET Framework tests (without TRX logger)
+set CmdToExec=%RunNetFrameworkTests% /TestAdapterPath:"%CD%"
+call :LogAndExecuteCommand "%RunMessage%" "%CmdToExec%"
+
+set RunMessage=Running .NET Core tests (with TRX logger, simulating a default Azure DevOps test run)
+set CmdToExec=%RunNetCoreTests% /TestAdapterPath:"%CD%" /Logger:"trx"
+call :LogAndExecuteCommand "%RunMessage%" "%CmdToExec%"
+
+set RunMessage=Running .NET Framework tests (with TRX logger, simulating a default Azure DevOps test run)
+set CmdToExec=%RunNetFrameworkTests% /TestAdapterPath:"%CD%" /Logger:"trx"
+call :LogAndExecuteCommand "%RunMessage%" "%CmdToExec%"
+
+set RunMessage=Running .NET Core tests (with TRX logger, simulating a default Azure DevOps test run + using a custom test adapter path)
+set CmdToExec=%RunNetCoreTests% /TestAdapterPath:"%CD%\ProjectANetCore" /Logger:"trx"
+call :LogAndExecuteCommand "%RunMessage%" "%CmdToExec%"
+
+set RunMessage=Running .NET Framework tests (with TRX logger, simulating a default Azure DevOps test run + using a custom test adapter path)
+set CmdToExec=%RunNetFrameworkTests% /TestAdapterPath:"%CD%\ProjectBNetFramework" /Logger:"trx"
+call :LogAndExecuteCommand "%RunMessage%" "%CmdToExec%"
 
 @echo.
 @echo * Test execution complete!
@@ -23,31 +37,28 @@ endlocal
 exit /b 0
 
 
-:RunTests <RunWithTrxLogger>
+:VerifyRuntimeEnvironment
 setlocal
-
-set RunWithTrxLogger=%1
-if "%RunWithTrxLogger%" equ "false" set RunMessage=without TRX logger
-if "%RunWithTrxLogger%" equ "true" (
-	set RunMessage=with TRX logger, simulating a default Azure DevOps test run
-	set TestAdapterLoggerOption=%TestAdapterTrxLoggerOption%
+@echo * Verifying runtime environment
+where %vstest%
+if %errorlevel% neq 0 (
+	@echo  - Failed to locate %vstest% - please run through a VS developer command prompt
+	exit /b %errorlevel%
 )
+endlocal
+goto:eof
 
-@echo.
-@echo.
-@echo.
-@echo ***************************************************************************************
-@echo * Running .NET Core tests (%RunMessage%)
-@echo ***************************************************************************************
-%RunNetCoreTests% %TestAdapterLoggerOption%
 
+:LogAndExecuteCommand <RunMessage> <CmdToExec>
+setlocal
+set RunMessage=%~1
+set CmdToExec=%~2
 @echo.
 @echo.
 @echo.
-@echo ***************************************************************************************
-@echo * Running .NET Framework tests (%RunMessage%)
-@echo ***************************************************************************************
-%RunNetFrameworkTests% %TestAdapterLoggerOption%
-
+@echo ***************************************************************************************************************************
+@echo * %RunMessage%
+@echo ***************************************************************************************************************************
+@echo  - Executing: %CmdToExec% & %CmdToExec%
 endlocal
 goto:eof
